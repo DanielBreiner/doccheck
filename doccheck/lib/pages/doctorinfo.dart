cd0ea8fcb824fd844c350b7a3da30a316fd659a1
Daniel Forrai <daniel.forrai@student.tuke.sk>
2020-09-05 22:18:57 +0200

    doctorinfo

--- doccheck/lib/pages/doctorinfo.dart
+++ doccheck/lib/pages/doctorinfo.dart
@@ -1,54 +1,25 @@
 import 'package:flutter/material.dart';
 import '../data.dart';
 
-class DoctorInfo extends StatefulWidget {
+class DoctorInfo extends StatelessWidget {
   final Doctor doctor;
-  DoctorInfo(this.doctor, {Key key}) : super(key: key);
+  DoctorInfo(this.doctor);
 
-  @override
-  _DoctorInfoState createState() => _DoctorInfoState();
-}
-
-class _DoctorInfoState extends State<DoctorInfo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(widget.doctor.name),
+          title: Text(doctor.name),
         ),
         body: Column(children: [
           Container(
               margin: EdgeInsets.symmetric(horizontal: 30, vertical: 60),
-              child: Text(
-                  widget.doctor.toNextAppointment(UserData()).toString() +
-                      " Days to next appointment")),
+              child: Text(doctor.toNextAppointment(UserData()).toString() +
+                  " Days to next appointment")),
           Container(
               margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-              child: Text(widget.doctor.description)),
+              child: Text(doctor.description)),
           Container()
         ]));
   }
 }
-
-// class DoctorInfo extends StatelessWidget {
-//   final Doctor doctor;
-//   DoctorInfo(this.doctor);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: Text(doctor.name),
-//         ),
-//         body: Column(children: [
-//           Container(
-//               margin: EdgeInsets.symmetric(horizontal: 30, vertical: 60),
-//               child: Text(doctor.toNextAppointment(UserData()).toString() +
-//                   " Days to next appointment")),
-//           Container(
-//               margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-//               child: Text(doctor.description)),
-//           Container()
-//         ]));
-//   }
-// }
