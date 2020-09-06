f481203a8e517d8084fd66da12fda723c30b11bc
DanielBreiner <danielbreinerd@gmail.com>
2020-09-06 08:04:04 +0200

    Push notifications working

--- doccheck/lib/push_notifications.dart
+++ doccheck/lib/push_notifications.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
 
 class PushNotificationsManager {
   PushNotificationsManager._();
@@ -11,11 +12,25 @@ class PushNotificationsManager {
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
   bool _initialized = false;
 
-  Future<void> init() async {
+  Future<void> init(Function messageCallback) async {
     if (!_initialized) {
       // For iOS request permission first.
-      _firebaseMessaging.requestNotificationPermissions();
-      _firebaseMessaging.configure();
+      // _firebaseMessaging.requestNotificationPermissions();
+
+      _firebaseMessaging.configure(
+        onMessage: (Map<String, dynamic> message) async {
+          print("onMessage: $message");
+          messageCallback(message);
+        },
+        onLaunch: (Map<String, dynamic> message) async {
+          print("onLaunch: $message");
+          messageCallback(message);
+        },
+        onResume: (Map<String, dynamic> message) async {
+          print("onResume: $message");
+          messageCallback(message);
+        },
+      );
 
       // For testing purposes print the Firebase Messaging token
       String token = await _firebaseMessaging.getToken();
